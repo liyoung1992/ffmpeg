465d93f94f4aac2371458e4c45767aadb4c17a96
zhangliyang <liyoung_1992@163.com>
2020-02-02 16:21:30 +0800

    基于sdl2.0 播放器

--- src/ffmpeg_in_action/ffmpeg_in_action/main.cpp
+++ src/ffmpeg_in_action/ffmpeg_in_action/main.cpp
@@ -1,11 +1,14 @@
 #include "ffmpeg_in_action.h"
 #include <QtWidgets/QApplication>
 #include "../../helloworld/helloworld.h"
+#include "../../ffmpeg_sdl_player/player.h"
 
 int main(int argc, char *argv[])
 {
 	//test ffmpeg
 	helloWorld();
+	//player
+	PalyerDemo();
 
 	QApplication a(argc, argv);
 	ffmpeg_in_action w;
